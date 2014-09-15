a60057a35a76cbcf8b49f73c8136ac4ca158f3c8
Stephen Young <StephenJayYoung@gmail.com>
2014-09-14 20:35:11 -0700

    redoing this, npm is making me go bananas. Added test and index

---
+++ index.js
@@ -0,0 +1,12 @@
+module.exports.gimmeFullName = function(person) {
+  var gimmeFullName = person[0] + person[1];
+   var person = {
+      "firstName": "Steve",
+      "lastName": "Young",
+ };
+};
+console.log(gimmeFullName);
+return gimmeFullName;
+
+
+
---
+++ test.js
@@ -0,0 +1,24 @@
+var chai = require("chai");
+var expect = chai.expect;
+var path = require("path");
+var gimmeFullName = require('./index');
+
+
+//@ param object which includes the properties first name and lastname
+//@return: return person's first and last name
+//
+//
+
+
+describe("gimmeFullName()", function() {
+  // given an object, it returns the objects first and last name
+  it("provides us with the objects first and last name", function() {
+    //adds the sum of firstName and lastName
+  expect(gimmeFullName).to.eql("Steve Young");  
+ });  });  
+
+ var person = {
+      "firstName": "Steve",
+      "lastName": "Young",
+ };
+
